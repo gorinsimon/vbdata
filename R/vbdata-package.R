c502e40355304663d33615a34de4c383cd77a6d7
gorinsimon <github.com.darkness667@passmail.net>
2026-09-06 14:38:35 +0200

    Add package-level documentation to declare global variables (solve "no visible binding" note)

---
+++ R/vbdata-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+utils::globalVariables(c(
+  "substitution",
+  "asked_substitution",
+  "time_out",
+  "asked_time_out",
+  "won",
+  "serving",
+  "receiving",
+  "point",
+  "rotation"
+))
+## usethis namespace: end
+NULL
